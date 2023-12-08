5c26830884f72d210bab4edd159c360da8d56840
Caio Alves <ktinsecrx2@gmail.com>
2023-12-08 00:48:34 -0300

    adding lottie player

---
+++ components/Atoms/LottiePlayer/Dot/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useScriptTag } from '@vueuse/core'
+interface IProps {
+  src: string
+}
+
+defineProps<IProps>()
+
+useScriptTag('https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs', () => null, {
+  type: 'module',
+})
+</script>
+
+<template>
+  <dotlottie-player
+    :src="src"
+    background="transparent"
+    speed="1"
+    direction="1"
+    mode="normal"
+    loop autoplay
+  />
+</template>
---
+++ components/Atoms/LottiePlayer/index.client.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import type { MaybeElement, MaybeElementRef } from '@vueuse/core'
+import { useIntersectionObserver } from '@vueuse/core'
+import type { LottieProps } from 'vue3-lottie'
+import { Vue3Lottie } from 'vue3-lottie'
+
+interface IProps extends Partial<LottieProps> {
+  src?: string
+}
+
+const props = defineProps<IProps>()
+
+const lottieRef = ref<InstanceType<typeof Vue3Lottie>>()
+
+useIntersectionObserver(
+  lottieRef as MaybeElementRef<MaybeElement>,
+  ([{ isIntersecting }]) => {
+    if (isIntersecting)
+      lottieRef.value?.play()
+
+    else
+      lottieRef.value?.pause()
+  },
+)
+</script>
+
+<template>
+  <Vue3Lottie
+    ref="lottieRef"
+
+    v-bind="props"
+  />
+</template>
