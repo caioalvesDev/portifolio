b7a606fbb9ad227d189a55aff39777a445f3d88d
Caio Alves <ktinsecrx2@gmail.com>
2023-12-07 22:21:07 -0300

    implementing more features and pages/blogs

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="flex flex-col min-h-screen">
+   <AtomsHeader />
+   <main class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 grow items-start w-full">
+     <NuxtPage />
+   </main>
+
+   <AtomsFooter />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@nuxt/content", "@nuxtjs/apollo"],
+  content: {
+    highlight: {
+      theme: "nord",
+      preload: ["ts", "js", "css", "java", "json", "bash", "vue", "php", "dart"],
+    },
+  },
+  runtimeConfig: {
+    githubToken: process.env.GITHUB_TOKEN
+  },
+  apollo: {
+    clients: {
+      default: {
+        tokenName: 'github-token',
+        httpEndpoint: 'https://api.github.com/graphql'
+      }
+    }
+  }
+});
