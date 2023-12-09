eeb02e77087d4326f82803e37a45923f1d8fe9d4
Caio Alves <ktinsecrx2@gmail.com>
2023-12-08 22:51:35 -0300

    change theme for highlight

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@nuxt/content", "@nuxtjs/apollo"],
   content: {
     highlight: {
-      theme: "nord",
+      theme: "dracula",
       preload: ["ts", "js", "css", "java", "json", "bash", "vue", "php", "dart"],
     },
   },
