e5a66dcad503fd1c1b07a9ce5d0ab571a13cb3f0
Caio Alves <ktinsecrx2@gmail.com>
2023-12-11 10:57:46 -0300

    fix(adjusts): adjusts in uses

--- pages/uses/index.vue
+++ pages/uses/index.vue
@@ -1,24 +1,25 @@
 <script setup>
 useHead({
   title: "Uses | Caio Alves Dev",
-})
-
+});
 </script>
 
 <template>
   <div>
     <h1 class="text-3xl font-bold my-8">Usos</h1>
     <p class="text-lg">
-      Muitas vezes sou questionado sobre o equipamento que utilizo como desenvolvedor de software e criador de conteúdo. O hardware e software
-      que utilizo estão mudando o tempo todo, então esta página servirá como local para uma lista atualizada. Se você tiver alguma dúvida
-      sobre algo que eu uso ou uma sugestão de algo que estou faltando, entre em contato comigo no instagram ou Linkedin.
+      Muitas vezes sou questionado sobre o equipamento que utilizo como desenvolvedor de software e criador de conteúdo. O hardware e
+      software que utilizo estão mudando o tempo todo, então esta página servirá como local para uma lista atualizada. Se você tiver alguma
+      dúvida sobre algo que eu uso ou uma sugestão de algo que estou faltando, entre em contato comigo no instagram ou Linkedin.
     </p>
     <h2 class="text-2xl font-bold mt-8 mb-2">Esse site</h2>
     <ul class="list-disc">
-      <li class="ml-8 py-1"><a href="https://vuejs.org/" target="_blank">Vue 3</a></li>
-      <li class="ml-8 py-1"><a href="https://nuxt.com/" target="_blank">Nuxt 3</a></li>
-      <li class="ml-8 py-1"><a href="https://tailwindcss.com/" target="_blank">Tailwind CSS</a></li>
-      <li class="ml-8 py-1">Hospedado em <a href="https://netlify.com/" target="_blank">Netlify</a></li>
+      <li class="ml-8 py-1"><a class="font-bold" href="https://vuejs.org/" target="_blank">Vue 3</a></li>
+      <li class="ml-8 py-1"><a class="font-bold" href="https://nuxt.com/" target="_blank">Nuxt 3</a></li>
+      <li class="ml-8 py-1"><a class="font-bold" href="https://tailwindcss.com/" target="_blank">Tailwind CSS</a></li>
+      <li class="ml-8 py-1">
+        Hospedado em <a class="font-bold" href="https://netlify.com/" target="_blank">Netlify</a> e <a class="font-bold" href="https://vercel.com/" target="_blank">Vercel</a>
+      </li>
     </ul>
     <h2 class="text-2xl font-bold mt-8 mb-2">Editor e Terminal</h2>
     <ul class="list-disc">
@@ -34,7 +35,13 @@ useHead({
       <!-- <li class="ml-8 py-1"><a href="https://www.iterm2.com/" aria-label="iTerm">iTerm2</a> is my Terminal</li> -->
       <li class="ml-8 py-1">
         Ícones de arquivos são
-        <a class="font-bold" href="https://marketplace.visualstudio.com/items?itemName=PKief.material-icon-theme" target="_blank" aria-label="Hack Font">Material Icon Theme</a>
+        <a
+          class="font-bold"
+          href="https://marketplace.visualstudio.com/items?itemName=PKief.material-icon-theme"
+          target="_blank"
+          aria-label="Hack Font"
+          >Material Icon Theme</a
+        >
       </li>
     </ul>
     <img src="~/assets/images/vs-code-theme.png" alt="Visual Studio Code Synthwave 84 Theme" class="mt-8 mb-20" />
